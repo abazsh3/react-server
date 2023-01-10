4d86962ef3421ae281f5f6f213781bb352b3a1b1
Abolfazl Shamsollahi <a.shamsollahi@citydi.com>
2023-01-10 17:24:55 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const path = require("path");
+const app = express(); // create express app
+
+// add middlewares
+app.use(express.static("build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// start express server on port 5000
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
